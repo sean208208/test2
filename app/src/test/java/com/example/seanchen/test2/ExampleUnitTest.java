7d1c455899fa0ab1a5004acfe460b3f9bb5e036e
sean.ju_chen@htc.com <sean.ju_chen@htc.com>
2019-05-02 17:36:29 +0800

    Initial commit

---
+++ app/src/test/java/com/example/seanchen/test2/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.seanchen.test2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void validateEmail() throws Exception {
+        assertSame(true, MainActivity.validateEmail("kevalpatel2106@gmail.com"));
+        assertSame(false, MainActivity.validateEmail("kevalpatel2106gmail.com"));
+        assertSame(false, MainActivity.validateEmail(""));
+        assertSame(false, MainActivity.validateEmail("k@com"));
+    }
+}
